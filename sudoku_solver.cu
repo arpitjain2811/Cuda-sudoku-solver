f580c2597c91701eb9af5f5a1d6b7f7faf85cd6e
Arpit Jain <arpitjain91@gmail.com>
2014-11-05 03:00:08 -0500

    Updated Test Inputs

--- sudoku_solver.cu
+++ sudoku_solver.cu
@@ -1,12 +1,9 @@
-/**
- * Copyright 1993-2012 NVIDIA Corporation.  All rights reserved.
- *
- * Please refer to the NVIDIA end user license agreement (EULA) associated
- * with this source code for terms and conditions that govern your use of
- * this software. Any use, reproduction, disclosure, or distribution of
- * this software and related documentation outside the terms of the EULA
- * is strictly prohibited.
- */
+//
+//  Cuda Sudoku Solver
+//
+//  Created by Arpit Jain
+//  Copyright (c) 2014 New York University. All rights reserved.
+//
 #include <stdio.h>
 #include <stdlib.h>
 #include<curand_kernel.h>
